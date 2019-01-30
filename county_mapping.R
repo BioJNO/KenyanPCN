53126717266fa0da34c89a853cbfa09ed0efc905
BioJNO <JamieNeilOrr@gmail.com>
2019-01-30 09:27:55 +0000

    upload kenyan mapping script

---
+++ county_mapping.R
@@ -0,0 +1,23 @@
+# Load required packages
+library(ggplot2)
+library(rgdal)
+library(maptools)
+library(ggmap)
+library(dplyr)
+library(reshape2)
+library(gridExtra)
+library(grid)
+
+# Plot a base R map of Kenya ----------------------------------------------
+# Read shapefile data.
+kenya = readOGR(dsn="KenyanCountyShapefile",
+                layer="County")
+kenya@data$id = rownames(kenya@data)
+kenya.points = fortify(kenya, region="id")
+kenya.df = left_join(kenya.points, kenya@data, by="id")
+
+# Plot the base map
+basemap = ggplot(kenya.df, aes(long, lat, group=group))
+basemap = basemap + geom_polygon(fill="gray75") + theme(panel.background = element_rect(fill = "gray95"))
+basemap = basemap + coord_equal()
+basemap
